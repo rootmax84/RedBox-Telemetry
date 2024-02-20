335a878683e229e5a3b0cd8c70b8e17eb96ad1ec
Trololoshka <rootmax84@gmail.com>
2024-02-20 11:02:36 +0300

    Fixed 'catch' redirection issue

--- catch.php
+++ catch.php
@@ -5,16 +5,14 @@
     </head>
     <body>
             <div class="login" id="login-form" style="width:400px; text-align:center; margin:5% auto; transition:.5s; opacity:0;">
-	    <?php
-		if ($_GET['c'] == "disabled") {
-		    header("Refresh:5; url=/?logout");
-	    ?>
+	<?php
+		if ($_GET['c'] == "disabled") { ?>
+		    <script>setTimeout(()=>{location.href='/?logout=true'}, 5000);</script>
 		<h4>Account is disabled.</h4>
 	    <?php
 	    }
-	    else if ($_GET['c'] == "loginfailed") {
-		    header("Refresh:2; url=/");
-	    ?>
+	    else if ($_GET['c'] == "loginfailed") { ?>
+		    <script>setTimeout(()=>{location.href='/'}, 2000);</script>
 		<h4>Wrong login, password or code!</h4>
 	    <?php
 	    }
