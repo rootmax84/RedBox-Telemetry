rootmax84/RedBox-Telemetry

commit 5b86c53c97d8fb88e74579ba12b95eb7b9aedadb

Time format settings moved to database
Execute this query after this commit

ALTER TABLE `users`
ADD `time` enum('24','12') COLLATE 'utf8mb4_unicode_ci' NOT NULL DEFAULT '24';

auth_functions.php
@@ -50,7 +50,7 @@ function auth_user()
     $pass = preg_replace('/\s+/', '', get_pass());
 
   try {
-      $userqry = $db->execute_query("SELECT user, pass, s FROM $db_users WHERE user=?", [$user]);
+      $userqry = $db->execute_query("SELECT user, pass, s, time FROM $db_users WHERE user=?", [$user]);
   } catch(Exception $e) { return; }
 	if (!$userqry->num_rows) return false;
 	else {
@@ -60,6 +60,8 @@ function auth_user()
 		$_SESSION['torque_pass'] = $row["pass"];
 		$_SESSION['torque_limit'] = $row["s"];
 		setcookie("stream", true);
+		setcookie("timeformat", $row["time"]);
+		$_COOKIE['timeformat'] = $row["time"];
 		$db->close();
 		return true;
 	    }

auth_user.php
@@ -34,7 +34,7 @@ if (!$logged_in && $captcha)
 $_SESSION['torque_logged_in'] = $logged_in;
 
 if (!$logged_in) {
-setcookie("stream", false);
+ setcookie("stream", "");
 ?><!DOCTYPE html>
 <html lang="en">
     <head>

creds.php.example
@@ -27,6 +27,7 @@ $def_limit = 100; //Default user db size limit MB
 
 $salt = ['cost' => 10]; //Password salt cost. Min/Max/Default 4/31/10. Higher value = more CPU usage on authorization process
 $admin = "admin";
+$admin_timeformat_12 = false; //Default 24 hours time format for admin
 
 if ($username == $admin) $_SESSION['admin'] = true;
 

del_sessions.php
@@ -81,8 +81,8 @@ if (isset($delsession)) {
 ?>
           <tr>
             <td><input type="checkbox" name="<?php echo $x['session']; ?>"></td>
-            <td id="start:<?php echo $x['session']; ?>"><?php echo date($_COOKIE['timeformat'], substr($x["timestart"], 0, -3)); ?></td>
-            <td id="end:<?php echo $x['session']; ?>"><?php echo date($_COOKIE['timeformat'], substr($x["timeend"], 0, -3)); ?></td>
+            <td id="start:<?php echo $x['session']; ?>"><?php echo date($_COOKIE['timeformat'] == "12" ? "F d, Y h:ia" : "F d, Y H:i", substr($x["timestart"], 0, -3)); ?></td>
+            <td id="end:<?php echo $x['session']; ?>"><?php echo date($_COOKIE['timeformat'] == "12" ? "F d, Y h:ia" : "F d, Y H:i", substr($x["timeend"], 0, -3)); ?></td>
             <td id="length:<?php echo $x['session']; ?>"><?php echo gmdate("H:i:s", intval(($x["timeend"] - $x["timestart"])/1000)); ?></td>
             <td id="size:<?php echo $x['session']; ?>"><?php echo $x["sessionsize"]; ?></td>
             <td id="profile:<?php echo $x['session']; ?>"><?php echo $x["profileName"]; ?></td>

get_sessions.php
@@ -97,7 +97,7 @@ while($row = $sessionqry->fetch_assoc()) {
     $session_ip = $row["ip"];
     $sid = $row["session"];
     $sids[] = preg_replace('/\D/', '', $sid);
-    $seshdates[$sid] = substr($sid, 0, -3);
+    $seshdates[$sid] = date($_COOKIE['timeformat'] == "12" ? "F d, Y h:ia" : "F d, Y H:i", substr($sid, 0, -3));
     $seshsizes[$sid] = " (Length $session_duration_str)";
     $seshprofile[$sid] = " ($session_profileName Profile)";
     $seship[$sid] = " (From ip $session_ip)";

head.php
@@ -15,6 +15,7 @@
 <link rel="stylesheet" href="static/css/chosen.min.css">
 <link rel="stylesheet" href="static/css/torque.css">
 <script src="static/js/jquery.min.js"></script>
+<script src="static/js/jquery.cookie.min.js"></script>
 <script src="static/js/jquery-ui.min.js"></script>
 <script src="static/js/bootstrap.min.js"></script>
 <script src="static/js/jquery.peity.min.js"></script>
@@ -27,16 +28,12 @@
 <script src="static/js/locate.js"></script>
 <script src="static/js/nosleep.js"></script>
 <script>
-    var timeformat = "";
-     if (localStorage.getItem("timeformat12")) timeformat = "F d, Y h:ia";
-     else timeformat = "F d, Y H:i";
-
     $(document).ready(function() {
             var visitortimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
             $.ajax({
                 type: "GET",
                 url: "timezone.php",
-                data: 'time=' + visitortimezone + '&format=' + timeformat,
+                data: 'time=' + visitortimezone
             });
       $("#theme-switch").click( function() {
        toggle_dark();

index.php
@@ -213,7 +213,7 @@ if (isset($sids[0])) {
 	     <select id="seshidtag" name="seshidtag" class="form-control chosen-select" onchange="document.getElementById('wait_layout').style.display = 'block';this.form.submit()" data-placeholder="Select Session...">
 	      <option value="" disabled>Select Session...</option>
 <?php foreach ($seshdates as $dateid => $datestr) { ?>
-	      <option value="<?php echo $dateid; ?>"<?php if ($dateid == $session_id) echo ' selected'; ?>><?php echo date($_COOKIE['timeformat'], $datestr); echo $seshprofile[$dateid]; if ($show_session_length) {echo $seshsizes[$dateid];} {echo $seship[$dateid];} ?><?php if ($dateid == $session_id) echo ' (Current Session)'; ?></option>
+	      <option value="<?php echo $dateid; ?>"<?php if ($dateid == $session_id) echo ' selected'; ?>><?php echo $datestr; echo $seshprofile[$dateid]; if ($show_session_length) {echo $seshsizes[$dateid];} {echo $seship[$dateid];} ?><?php if ($dateid == $session_id) echo ' (Current Session)'; ?></option>
 <?php } ?>
 	    </select>
 <?php   if ( $filterprofile <> "" ) { ?>
@@ -554,7 +554,7 @@ $r = $db->query("SELECT user, s FROM $db_users LIMIT " . $page_first_result . ",
 	    $last = $db->query("SELECT time FROM ".$row["user"].$db_log_prefix." ORDER BY time DESC LIMIT 1")->fetch_array();
 	    if ($last) {
 	     $seconds = intval($last[0]/1000);
-	     $last = date("d.m.Y H:i:s",$seconds);
+	     $last = date($admin_timeformat_12 ? "d.m.Y h:i:sa" : "d.m.Y H:i:s", $seconds);
 	    }
 	}
 	echo "<tr>";

merge_sessions.php
@@ -105,8 +105,8 @@ if (isset($mergesession) && !empty($mergesession) && isset($mergesess1) && !empt
 ?>
           <tr>
             <td><input type="checkbox" name="<?php echo $x['session']; ?>" <?php if ($x['session'] == $mergesession) { echo "checked disabled"; } ?>></td>
-            <td id="start:<?php echo $x['session']; ?>"><?php echo date($_COOKIE['timeformat'], substr($x["timestart"], 0, -3)); ?></td>
-            <td id="end:<?php echo $x['session']; ?>"><?php echo date($_COOKIE['timeformat'], substr($x["timeend"], 0, -3)); ?></td>
+            <td id="start:<?php echo $x['session']; ?>"><?php echo date($_COOKIE['timeformat'] == "12" ? "F d, Y h:ia" : "F d, Y H:i", substr($x["timestart"], 0, -3)); ?></td>
+            <td id="end:<?php echo $x['session']; ?>"><?php echo date($_COOKIE['timeformat'] == "12" ? "F d, Y h:ia" : "F d, Y H:i", substr($x["timeend"], 0, -3)); ?></td>
             <td id="length:<?php echo $x['session']; ?>"><?php echo gmdate("H:i:s", intval(($x["timeend"] - $x["timestart"])/1000)); ?></td>
             <td id="size:<?php echo $x['session']; ?>"><?php echo $x["sessionsize"]; ?></td>
             <td id="profile:<?php echo $x['session']; ?>"><?php echo $x["profileName"]; ?></td>

commit fe256e965ae1d8eb9c61f9f5b250a650e01642cc

Oops. Broken install after commit 5b86c53
fixed

auth_functions.php
@@ -89,6 +89,7 @@ function create_users_table()
 	temp enum('No conversion','Celsius to Fahrenheit','Fahrenheit to Celsius') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'No conversion',
 	pressure enum('No conversion','Psi to Bar','Bar to Psi') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'No conversion',
 	boost enum('No conversion','Psi to Bar','Bar to Psi') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'No conversion',
+	time enum('24','12') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '24',
 	PRIMARY KEY (id),
 	UNIQUE KEY user (user),
 	UNIQUE KEY token (token),