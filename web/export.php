ace7fdf3ca4d90bb2cd82a2386ba743f3052132f
Trololoshka <rootmax84@gmail.com>
2024-08-08 15:40:57 +0300

    export performance improvements

--- web/export.php
+++ web/export.php
@@ -15,7 +15,6 @@ if (isset($_GET["sid"]) && $_GET["sid"]) {
 
 	if ($_GET["filetype"] == "kml") {
 		$sql = $db->execute_query("SELECT kff1005,kff1006,kff1007 FROM $db_table join $db_sessions_table on $db_table.session = $db_sessions_table.session WHERE $db_table.session=? AND kff1005 > 0 ORDER BY $db_table.time DESC", [$session_id]);
-		$columns_total = $sql->field_count;
 
 		$output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
 		$output .="\n";
@@ -35,13 +34,8 @@ if (isset($_GET["sid"]) && $_GET["sid"]) {
 		$output .="\n";
 
 		// Get Records from the table
-		while ($row = $sql->fetch_array()) {
-
-			for ($i = 0; $i < $columns_total; $i++) {
-				$output .=$row["$i"].',';
-			}
-			$output = rtrim($output, ",");
-			$output .="\n";
+		while ($row = $sql->fetch_array(MYSQLI_NUM)) {
+		    $output .= implode(',', $row) . "\n";
 		}
 		$output .="</coordinates>";
 		$output .="\n";
@@ -56,6 +50,7 @@ if (isset($_GET["sid"]) && $_GET["sid"]) {
 		$kmlfilename = "log_session_".$session_id.".kml";
 		header('Content-type: application/kml');
 		header('Content-Disposition: attachment; filename='.$kmlfilename);
+		header('Content-Length: ' . strlen($output));
 
 		echo $output;
 		exit;
@@ -67,24 +62,21 @@ if (isset($_GET["sid"]) && $_GET["sid"]) {
 
 		// Get The Field Name
 	        $properties = $sql->fetch_fields();
-		foreach ($properties as $property) {
-		$p = $property->name;
-		    $output .='"'.$property->name.'",';
-		}
-		$output .="\n";
+		$headers = array_map(function($property) {
+		    return $property->name;
+		}, $properties);
+		$output = '"' . implode('","', $headers) . '",' . "\n";
 
 		// Get Records from the table
-		while ($row = $sql->fetch_array()) {
-			for ($i = 0; $i < $columns_total; $i++) {
-			     $output .='"'.$row["$i"].'",';
-			}
-			$output .="\n";
+		while ($row = $sql->fetch_array(MYSQLI_NUM)) {
+		    $output .= '"' . implode('","', $row) . '",' . "\n";
 		}
 
 		// Download the file
 		$csvfilename = "log_session_".$session_id.".csv";
 		header('Content-type: application/csv');
 		header('Content-Disposition: attachment; filename='.$csvfilename);
+		header('Content-Length: ' . strlen($output));
 
 		echo $output;
 		exit;
@@ -92,32 +84,24 @@ if (isset($_GET["sid"]) && $_GET["sid"]) {
 
 	else if ($_GET["filetype"] == "txt") { //RedManage session export
 		try {
-		    $sql = $db->execute_query("SELECT $db_table.time,k5,k5c,kf,kb4,k46,k2101,kd,kc,kb,k10,k11,ke,k2112,k2100,k2113,k21cc,kff1214,kff1218,k78,k2111,k2119,k1f,k2118,k2120,k2122,k2124,k21e1,k21e2,k2125,k2126,kff1238,k21fa,kff1006,kff1005,kff1001,kff120c FROM $db_table join $db_sessions_table on $db_table.session = $db_sessions_table.session WHERE $db_table.session=? AND $db_sessions_table.id=? ORDER BY $db_table.time ASC", [$session_id, "RedManage"]);
+		    $sql = $db->execute_query("SELECT $db_table.time,k5,k5c,kf,kb4,k46,k2101,kd,kc,kb,k10,k11,ke,k2112,k2100,k2113,k21cc,kff1214,kff1218,k78,k2111,k2119,k1f,k2118,k2120,k2122,k2124,k21e1,k21e2,k2125,k2126,kff1238,k21fa,kff1006,kff1005,kff1001,kff120c FROM $db_table JOIN $db_sessions_table ON $db_table.session = $db_sessions_table.session WHERE $db_table.session=? AND $db_sessions_table.id=? ORDER BY $db_table.time ASC", [$session_id, "RedManage"]);
 		} catch (Exception $e) {}
 
-		$columns_total = $sql->field_count;
-		$rows_total = $sql->num_rows;
-
-		if ($rows_total) {
-		    // Field Name
+		if ($sql->num_rows > 0) { //header
 		    $output = "TIME ECT EOT IAT ATF AAT EXT SPD RPM MAP MAF TPS IGN INJ INJD IAC AFR O2S O2S2 EGT EOP FP ERT MHS BSTD FAN GEAR BS1 BS2 PG0 PG1 VLT RLC GLAT GLON GSPD ODO\n";
 
-		// Get Records from the table
-		    while ($row = $sql->fetch_array()) {
-			for ($i = 0; $i < $columns_total; $i++) {
-			    $output .= $row["$i"].' ';
-			}
-			    $output = rtrim($output);
-			    $output .="\n";
+		    while ($row = $sql->fetch_array(MYSQLI_NUM)) {
+			$output .= implode(' ', $row) . "\n";
 		    }
 		} else {
 		    $output = "This is not RedManage session";
 		}
 
 		// Download the file
-		$txtfilename = "rbx_log_".$session_id.".txt";
-		header('Content-type: application/txt');
-		header('Content-Disposition: attachment; filename='.$txtfilename);
+		$txtfilename = "rbx_log_{$session_id}.txt";
+		header('Content-Type: application/txt');
+		header('Content-Disposition: attachment; filename=' . $txtfilename);
+		header('Content-Length: ' . strlen($output));
 
 		echo $output;
 		exit;
@@ -130,12 +114,13 @@ if (isset($_GET["sid"]) && $_GET["sid"]) {
 			$rows[] = $r;
 		}
 
-		$jsonrows = json_encode($rows);
+		$jsonrows = json_encode($rows, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
 		// Download the file
 		$jsonfilename = "log_session_".$session_id.".json";
 		header('Content-type: application/json');
 		header('Content-Disposition: attachment; filename='.$jsonfilename);
+		header('Content-Length: ' . strlen($jsonrows));
 
 		echo $jsonrows;
 	}
