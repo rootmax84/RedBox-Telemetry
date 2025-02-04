9028220f2b1ca93806dd9cb15aeae0ed34d514f9
Trololoshka <rootmax84@gmail.com>
2025-02-04 13:15:22 +0300

    login page lang switch

--- web/auth_user.php
+++ web/auth_user.php
@@ -40,6 +40,15 @@ if (!$logged_in) {
     ?>
     <body style="display:flex; justify-content:center; align-items:center; height:100vh">
     <div class="login login-form" id="login-form">
+        <div class="login-lang" id="lang-switch">
+          <div class="selected-lang" id="selected-lang"></div>
+          <ul class="lang-options" id="lang-options">
+            <li data-value="en">English</li>
+            <li data-value="ru">Русский</li>
+            <li data-value="es">Español</li>
+            <li data-value="de">Deutsch</li>
+          </ul>
+        </div>
         <div style="font-weight:bold; color:red; text-align:center; width:100%; font-size:16px; letter-spacing:1.5px">RedB<img src="static/img/logo.svg" alt style="height:10px; width:12px; margin-right:1px">x Telemetry</div>
         <h6 style="text-align:center; margin-bottom:20px" l10n="login.label"></h6>
         <form method="post" class="form-group" action=".">
@@ -57,6 +66,45 @@ if (!$logged_in) {
                 $("#login-btn").prop('disabled', true);
                 $("#login-btn").html('<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><circle cx="18" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin=".67" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/></circle><circle cx="12" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin=".33" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/></circle><circle cx="6" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin="0" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/></circle></svg>');
             });
+
+          const langSwitch = document.getElementById('lang-switch');
+          const selectedLang = document.getElementById('selected-lang');
+          const langOptions = document.getElementById('lang-options');
+
+          function closeDropdown() {
+            langOptions.classList.remove('show');
+          }
+
+          selectedLang.addEventListener('click', function(event) {
+            event.stopPropagation();
+            if (langOptions.classList.contains('show')) {
+              closeDropdown();
+            } else {
+              langOptions.classList.add('show');
+            }
+          });
+
+          langOptions.querySelectorAll('li').forEach(option => {
+            option.addEventListener('click', function() {
+              const selectedValue = this.getAttribute('data-value');
+              const selectedText = this.textContent;
+              closeDropdown();
+
+              fetch(`translations.php?lang=${selectedValue}`)
+                .then(() => {
+                  return localization.setLang(selectedValue);
+                })
+                .catch(error => {
+                  console.error('Error:', error);
+                });
+            });
+          });
+
+          document.addEventListener('click', function(event) {
+            if (!langSwitch.contains(event.target)) {
+              closeDropdown();
+            }
+          });
         });
     </script>
     </body>
