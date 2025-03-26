e837f47bb042639f513564286ac4862973ae599f
rootmax84@gmail.com <rootmax84@gmail.com>
2025-03-26 11:11:00 +0300

    Update catch.php

--- web/catch.php
+++ web/catch.php
@@ -1,39 +1,51 @@
-<?php include("head.php");?>
-    <body style="display:flex; justify-content:center; align-items:center; height:100vh">
-        <div class="login login-form" id="login-form" style="width:fit-content; text-align:center">
-	<?php
-	    if ($_GET['c'] == "disabled") { ?>
-		<script>setTimeout(()=>{location.href='.?logout=true'}, 5000);</script>
-		<h4 l10n='catch.disabled'></h4>
-	    <?php
-	    }
-	    else if ($_GET['c'] == "loginfailed") { http_response_code(401); ?>
-		<script>setTimeout(()=>{location.href='.'}, 2000);</script>
-		<h4 l10n='catch.loginfailed'></h4>
-	    <?php
-	    }
-	    else if ($_GET['c'] == "csrffailed") { http_response_code(401); ?>
-		<script>setTimeout(()=>{location.href='.'}, 2000);</script>
-		<h4 l10n='catch.csrf'></h4>
-	    <?php
-	    }
-	    else if ($_GET['c'] == "toomanyattempts") { http_response_code(401); ?>
-		<script>setTimeout(()=>{location.href='.'}, 5000);</script>
-		<h4 style="line-height:1.5" l10n='catch.banned'></h4>
-	    <?php
-	    }
-	    else if ($_GET['c'] == "dberror") { http_response_code(503); ?>
-		<script>setTimeout(()=>{location.href='.'}, 10000);</script>
-		<h4 l10n='catch.dberror'></h4>
-	    <?php
-	    }
-	    else if ($_GET['c'] == "maintenance") { http_response_code(423); ?>
-		<script>setTimeout(()=>{location.href='.'}, 10000);</script>
-		<h4 style="line-height:1.5" l10n='catch.maintenance'></h4>
-	    <?php
-	    }
-	?>
-        </div>
+<?php
+if (isset($_GET['c'])) {
+    $http_code = match($_GET['c']) {
+        'loginfailed', 'csrffailed', 'toomanyattempts' => 401,
+        'dberror' => 503,
+        'maintenance' => 423,
+        default => 200,
+    };
+    http_response_code($http_code);
+}
+
+include("head.php");
+?>
+<body style="display:flex; justify-content:center; align-items:center; height:100vh">
+    <div class="login login-form" id="login-form" style="width:fit-content; text-align:center">
+    <?php
+        if ($_GET['c'] == "disabled") { ?>
+            <script>setTimeout(()=>{location.href='.?logout=true'}, 5000);</script>
+            <h4 l10n='catch.disabled'></h4>
+        <?php
+        }
+        else if ($_GET['c'] == "loginfailed") { ?>
+            <script>setTimeout(()=>{location.href='.'}, 2000);</script>
+            <h4 l10n='catch.loginfailed'></h4>
+        <?php
+        }
+        else if ($_GET['c'] == "csrffailed") { ?>
+            <script>setTimeout(()=>{location.href='.'}, 2000);</script>
+            <h4 l10n='catch.csrf'></h4>
+        <?php
+        }
+        else if ($_GET['c'] == "toomanyattempts") { ?>
+            <script>setTimeout(()=>{location.href='.'}, 5000);</script>
+            <h4 style="line-height:1.5" l10n='catch.banned'></h4>
+        <?php
+        }
+        else if ($_GET['c'] == "dberror") { ?>
+            <script>setTimeout(()=>{location.href='.'}, 10000);</script>
+            <h4 l10n='catch.dberror'></h4>
+        <?php
+        }
+        else if ($_GET['c'] == "maintenance") { ?>
+            <script>setTimeout(()=>{location.href='.'}, 10000);</script>
+            <h4 style="line-height:1.5" l10n='catch.maintenance'></h4>
+        <?php
+        }
+    ?>
+    </div>
     <div class="login-background"></div>
    <script>
     $(document).ready(function(){
