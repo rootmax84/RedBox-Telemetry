fcc6a8300fb39e54aadd1c438710ba0a61d6d06a
rootmax84@gmail.com <rootmax84@gmail.com>
2026-08-29 13:10:31 +0300

    Highlight nearest metric on plot

--- web/static/js/jquery.flot.multihighlight-delta.js
+++ web/static/js/jquery.flot.multihighlight-delta.js
@@ -14,7 +14,9 @@ function convertToRealTime(processedTime) {
 
   const timeMapping = window.realTimeInfo.timeMapping;
   if (!timeMapping._sortedProcessedTimes) {
-    timeMapping._sortedProcessedTimes = Object.keys(timeMapping).map(Number).sort((a, b) => a - b);
+    timeMapping._sortedProcessedTimes = Object.keys(timeMapping)
+      .map(Number)
+      .sort((a, b) => a - b);
   }
 
   const processedTimes = timeMapping._sortedProcessedTimes;
@@ -37,96 +39,85 @@ function convertToRealTime(processedTime) {
 }
 
 (function (name, definition) {
-  var theModule = definition(),
-  // this is considered "safe":
-      hasDefine = typeof define === 'function' && define.amd,
-  // hasDefine = typeof define === 'function',
-      hasExports = typeof module !== 'undefined' && module.exports;
+  const theModule = definition();
+  const hasDefine = typeof define === 'function' && define.amd;
+  const hasExports = typeof module !== 'undefined' && module.exports;
 
-  if (hasDefine) { // AMD Module
+  if (hasDefine) {
     define(theModule);
-  } else if (hasExports) { // Node.js Module
+  } else if (hasExports) {
     module.exports = theModule;
-  } else { // Assign to common namespaces or simply the global object (window)
+  } else {
     (this.jQuery || this.ender || this.$ || this)[name] = theModule;
   }
 })('core', function () {
-
-  var MultiHighlightDelta = {
+  const MultiHighlightDelta = {
     options: {
       multihighlightdelta: {
         mode: 'x',
         tooltipOffsetX: 20,
         tooltipOffsetY: 20,
         tooltipTemplate: '<table class="table flot-tooltip-table"><thead><tr><th><%= time[0] %></th><th><%= value %></th><th><%= change %></th><th><%= event %></th></tr></thead><tbody><%= body %></tbody></table>',
-        dataPointTemplate: '<tr><td><%= series.label %></td><td><%= datapoint[1] %></td><td><%= (delta > 0 ? "+" : "") %><%= delta %></td><td><%= rlbc %></td></tr>',
+        dataPointTemplate: '<tr style="<% if (isClosest) { %>font-weight:bold;<% } %>"><td><%= series.label %></td><td><%= datapoint[1] %></td><td><%= (delta > 0 ? "+" : "") %><%= delta %></td><td><%= rlbc %></td></tr>',
         transformDataPointData: false,
         tooltipStyles: {
           position: 'absolute',
           display: 'none',
-          'background': '#fff',
+          background: '#fff',
           'z-index': '100',
-          'padding': '0.4em 0.6em',
+          padding: '0.4em 0.6em',
           'border-radius': '0.5em',
           'font-size': '0.8em',
-          'border': '1px dashed rgba(0,0,0,.4)'
+          border: '1px dashed rgba(0,0,0,.4)'
         },
         delta: function (previousDataPoint, dataPoint) {
           if (!previousDataPoint) {
             return '';
           }
-          var chng = dataPoint[1] - previousDataPoint[1];
-
-          return (-1*chng).toFixed(2);
+          const change = dataPoint[1] - previousDataPoint[1];
+          return (-1 * change).toFixed(2);
         }
       }
     }
   };
-  var MultiHighlightDeltaPlugin = function (plot) {
-    this.plot = plot;
-  };
 
-  /**
-   * Thanks John Resig! [ http://ejohn.org/blog/javascript-micro-templating/ ]
-   * Based heavily off john's implementation , but removed caching
-   */
-  var compileTemplate = function (str) {
+  class MultiHighlightDeltaPlugin {
+    constructor(plot) {
+      this.plot = plot;
+    }
 
-    // Generate a reusable function that will serve as a template
-    // generator (and should be cached by its caller).
-    /* jshint -W121 */
-    return new Function("obj",
+    static compileTemplate(str) {
+      return new Function(
+        'obj',
         "var p=[],print=function(){p.push.apply(p,arguments);};" +
+          "with(obj){p.push('" +
+          str
+            .replace(/[\r\t\n]/g, ' ')
+            .split('<%').join('\t')
+            .replace(/((^|%>)[^\t]*)'/g, '$1\r')
+            .replace(/\t=(.*?)%>/g, "',$1,'")
+            .split('\t').join("');")
+            .split('%>').join("p.push('")
+            .split('\r').join("\\'") +
+          "');}return p.join('');"
+      );
+    }
 
-          // Introduce the data as local variables using with(){}
-            "with(obj){p.push('" +
+    initialize() {
+      const ctx = this;
 
-          // Convert the template into pure JavaScript
-            str.replace(/[\r\t\n]/g, " ")
-                .split("<%").join("\t")
-                .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-                .replace(/\t=(.*?)%>/g, "',$1,'")
-                .split("\t").join("');")
-                .split("%>").join("p.push('")
-                .split("\r").join("\\'") + "');}return p.join('');");
-  };
-
-  MultiHighlightDeltaPlugin.prototype = {
-    initialize: function () {
-      var ctx = this;
-
-      var handlerProxies = {
-        onPlotHover: $.proxy(ctx.onPlotHover, ctx),
-        onMouseOut: $.proxy(ctx.onMouseOut, ctx)
+      const handlerProxies = {
+        onPlotHover: (event, position, item) => ctx.onPlotHover(event, position, item),
+        onMouseOut: () => ctx.onMouseOut()
       };
 
-      this.plot.hooks.bindEvents.push(function (plot) {
+      this.plot.hooks.bindEvents.push((plot) => {
         if (!plot.getOptions().multihighlightdelta) {
           return;
         }
 
-        var options = ctx.plot.getOptions().multihighlightdelta || {};
-        for (var key in MultiHighlightDelta.options.multihighlightdelta) {
+        const options = plot.getOptions().multihighlightdelta || {};
+        for (const key in MultiHighlightDelta.options.multihighlightdelta) {
           if (typeof options[key] === 'undefined') {
             options[key] = MultiHighlightDelta.options.multihighlightdelta[key];
           }
@@ -135,144 +126,171 @@ function convertToRealTime(processedTime) {
         plot.getPlaceholder().on('plothover plottouchmove', handlerProxies.onPlotHover);
         plot.getPlaceholder().on('mouseout touchend', handlerProxies.onMouseOut);
 
-        // Keep a cache of the template
-        ctx.tooltipTemplate = compileTemplate(options.tooltipTemplate);
-        ctx.dataPointTemplate = compileTemplate(options.dataPointTemplate);
-
+        // Keep a cache of the templates
+        ctx.tooltipTemplate = MultiHighlightDeltaPlugin.compileTemplate(options.tooltipTemplate);
+        ctx.dataPointTemplate = MultiHighlightDeltaPlugin.compileTemplate(options.dataPointTemplate);
       });
-      this.plot.hooks.shutdown.push(function (plot) {
+
+      this.plot.hooks.shutdown.push((plot) => {
         plot.getPlaceholder().off('plothover plottouchmove', handlerProxies.onPlotHover);
         plot.getPlaceholder().off('mouseout touchend', handlerProxies.onMouseOut);
       });
 
       return this;
-    },
-    findOrCreateTooltip: function (tooltipStyles) {
-      var $tip = null;
-      if ($('#flotMultihighlightTip').length > 0) {
-        $tip = $('#flotMultihighlightTip');
-      }
-      else {
-        $tip = $('<div />').attr('id', 'flotMultihighlightTip').addClass('flot-tooltip').css(tooltipStyles).appendTo('body');
+    }
+
+    findOrCreateTooltip(tooltipStyles) {
+      let $tip = $('#flotMultihighlightTip');
+      if ($tip.length === 0) {
+        $tip = $('<div />')
+          .attr('id', 'flotMultihighlightTip')
+          .addClass('flot-tooltip')
+          .css(tooltipStyles)
+          .appendTo('body');
       }
       return $tip;
-    },
-    onPlotHover: function (event, position, item) {
-      var data = this.plot.getData();
-      var options = this.plot.getOptions().multihighlightdelta;
-      var deltaFunction = options.delta;
-      var mode = options.mode || 'x';
-      var index = 0;
+    }
+
+    onPlotHover(event, position, item) {
+      const data = this.plot.getData();
+      const options = this.plot.getOptions().multihighlightdelta;
+      const deltaFunction = options.delta;
+      const mode = options.mode || 'x';
+      let index = 0;
+
       if (mode === 'x') {
         index = 0;
       } else if (mode === 'y') {
         index = 1;
       } else {
-        throw new Error('Mode \'' + mode + '\', is not recognized, must be x or y');
+        throw new Error(`Mode '${mode}' is not recognized, must be x or y`);
       }
 
-      if (item) {
+      if (!item) return;
 
-        this.plot.unhighlight();
-        var matchingDataPoints = [];
-        var showEventHeader = false;
+      this.plot.unhighlight();
+      const matchingDataPoints = [];
+      let showEventHeader = false;
 
-        for (var i = 0 , ii = data.length; i < ii; i++) {
-          // Find the data point in the other series that matches the current datapoint
-          var seriesData = data[i].data;
-          for (var j = 0 , jj = seriesData.length; j < jj; j++) {
-            if (seriesData[j][index] === item.datapoint[index]) {
-              matchingDataPoints.push({
-                seriesData: data[i],
-                dataPoint: seriesData[j],
-                delta: deltaFunction(j > 0 ? seriesData[j - 1] : null, seriesData[j])
-              });
-              if (data[i]['label'].includes('Rollback')) {
-                showEventHeader = true;
-              }
+      for (const series of data) {
+        const seriesData = series.data;
+        for (let j = 0; j < seriesData.length; j++) {
+          if (seriesData[j][index] === item.datapoint[index]) {
+            matchingDataPoints.push({
+              seriesData: series,
+              dataPoint: seriesData[j],
+              delta: deltaFunction(j > 0 ? seriesData[j - 1] : null, seriesData[j])
+            });
+            if (series.label.includes('Rollback')) {
+              showEventHeader = true;
             }
           }
         }
+      }
 
-        var childrenTexts = [];
-		// 2015.08.17 - edit by surfrock66 - define variable array to display the time in the tooltip
-		var timeArray = [];
-        for (var i = 0 , ii = matchingDataPoints.length; i < ii; i++) {
-          var seriesData = matchingDataPoints[i].seriesData;
-          var dataPoint = matchingDataPoints[i].dataPoint;
-          var delta = matchingDataPoints[i].delta;
-          this.plot.highlight(seriesData, dataPoint);
+      // Compute the cursor's Y position inside the canvas
+      const axes = this.plot.getAxes();
+      const placeholder = this.plot.getPlaceholder();
+      const offset = placeholder.offset();
+      const plotOffset = this.plot.getPlotOffset();
+      const canvasY = position.pageY - offset.top - plotOffset.top;
 
-	if (seriesData['label'].includes('Rollback')) {
-	     rlbc = calculate(dataPoint[1]);
-	}
-	else rlbc = "";
+      let closestIndex = -1;
+      let minDist = Infinity;
+      for (let i = 0; i < matchingDataPoints.length; i++) {
+        const yVal = matchingDataPoints[i].dataPoint[1];
+        const pixelY = axes.yaxis.p2c(yVal);
+        const dist = Math.abs(canvasY - pixelY);
+        if (dist < minDist) {
+          minDist = dist;
+          closestIndex = i;
+        }
+      }
 
-          var data = {
-            series: seriesData,
-            datapoint: dataPoint,
-            delta: delta,
-	    rlbc: rlbc
-          };
-          if (options.transformDataPointData){
-            data = options.transformDataPointData(data);
-          }
-	  var text = this.dataPointTemplate(data);
-		  childrenTexts.push(text);
-		  // Convert time and format it
-		  var realTimestamp = convertToRealTime(dataPoint[0]);
-		  var xDateFormat = Cookies.get('timeformat') == '12' ? "%d/%m/%Y  %I:%M:%S%p" : "%d/%m/%Y  %H:%M:%S";
-                  timeArray[0] = $.plot.formatDate(realTimestamp, xDateFormat);
+      const childrenTexts = [];
+      const timeArray = [];
+
+      for (let i = 0; i < matchingDataPoints.length; i++) {
+        const seriesData = matchingDataPoints[i].seriesData;
+        const dataPoint = matchingDataPoints[i].dataPoint;
+        const delta = matchingDataPoints[i].delta;
+        this.plot.highlight(seriesData, dataPoint);
+
+        let rlbc = '';
+        if (seriesData.label.includes('Rollback')) {
+          rlbc = calculate(dataPoint[1]);
         }
 
-        var tooltipText = this.tooltipTemplate({
-          value : localization.key['chart.val'],
-          change: localization.key['chart.change'],
-          event : showEventHeader ? localization.key['chart.event'] : null,
-          time: timeArray,
-          body: childrenTexts.join('\n')
-        });
-
-        var $tooltip = this.findOrCreateTooltip(options.tooltipStyles);
-
-        // If we are going to overflow outside the screen's dimensions, display it to the left instead
-
-
-        var xPositionProperty = 'left';
-        var yPositionProperty = 'top';
-        var xPosition = position.pageX + options.tooltipOffsetX;
-        var yPosition = position.pageY + options.tooltipOffsetY;
-        $tooltip.html(tooltipText); // So that we can use dimensions right away, we set the content immediately
-        var tooltipWidth = $tooltip.width();
-        var tooltipHeight = $tooltip.height();
-        var css = {
-          top: 'auto',
-          left: 'auto',
-          right: 'auto',
-          bottom: 'auto'
+        let templateData = {
+          series: seriesData,
+          datapoint: dataPoint,
+          delta,
+          rlbc,
+          isClosest: i === closestIndex
         };
 
-        var pageWidth = window.innerWidth;
-        var pageHeight = window.innerHeight;
-        var plotWidth = $("#placeholder").width();
-
-        css['width'] =  plotWidth <= 650 ? 'min-content' : 'auto';
-
-        if (xPosition + tooltipWidth > plotWidth){
-          xPositionProperty = 'right';
-          xPosition = pageWidth - position.pageX + options.tooltipOffsetX;
-        }
-        if (yPosition + tooltipHeight > pageHeight){
-          yPositionProperty = 'bottom';
-          yPosition = pageHeight - position.pageY + options.tooltipOffsetY;
+        if (options.transformDataPointData) {
+          templateData = options.transformDataPointData(templateData);
         }
 
-        css[xPositionProperty] = xPosition;
-        css[yPositionProperty] = yPosition;
-        $tooltip.css(css).show();
+        const text = this.dataPointTemplate(templateData);
+        childrenTexts.push(text);
+
+        // Convert time and format it
+        const realTimestamp = convertToRealTime(dataPoint[0]);
+        const xDateFormat = Cookies.get('timeformat') == '12'
+          ? '%d/%m/%Y  %I:%M:%S%p'
+          : '%d/%m/%Y  %H:%M:%S';
+        timeArray[0] = $.plot.formatDate(realTimestamp, xDateFormat);
       }
-    },
-    onMouseOut: function () {
+
+      const tooltipText = this.tooltipTemplate({
+        value: localization.key['chart.val'],
+        change: localization.key['chart.change'],
+        event: showEventHeader ? localization.key['chart.event'] : null,
+        time: timeArray,
+        body: childrenTexts.join('\n')
+      });
+
+      const $tooltip = this.findOrCreateTooltip(options.tooltipStyles);
+
+      // If we are going to overflow outside the screen's dimensions, display it to the left instead
+      let xPositionProperty = 'left';
+      let yPositionProperty = 'top';
+      let xPosition = position.pageX + options.tooltipOffsetX;
+      let yPosition = position.pageY + options.tooltipOffsetY;
+
+      $tooltip.html(tooltipText); // So that we can use dimensions right away
+      const tooltipWidth = $tooltip.width();
+      const tooltipHeight = $tooltip.height();
+      const css = {
+        top: 'auto',
+        left: 'auto',
+        right: 'auto',
+        bottom: 'auto'
+      };
+
+      const pageWidth = window.innerWidth;
+      const pageHeight = window.innerHeight;
+      const plotWidth = $('#placeholder').width();
+
+      css.width = plotWidth <= 650 ? 'min-content' : 'auto';
+
+      if (xPosition + tooltipWidth > plotWidth) {
+        xPositionProperty = 'right';
+        xPosition = pageWidth - position.pageX + options.tooltipOffsetX;
+      }
+      if (yPosition + tooltipHeight > pageHeight) {
+        yPositionProperty = 'bottom';
+        yPosition = pageHeight - position.pageY + options.tooltipOffsetY;
+      }
+
+      css[xPositionProperty] = xPosition;
+      css[yPositionProperty] = yPosition;
+      $tooltip.css(css).show();
+    }
+
+    onMouseOut() {
       this.plot.unhighlight();
       $('#flotMultihighlightTip').hide().css({
         top: 'auto',
@@ -281,7 +299,7 @@ function convertToRealTime(processedTime) {
         bottom: 'auto'
       });
     }
-  };
+  }
 
   MultiHighlightDelta.init = function (plot) {
     new MultiHighlightDeltaPlugin(plot).initialize();
@@ -292,10 +310,9 @@ function convertToRealTime(processedTime) {
     init: MultiHighlightDelta.init,
     options: MultiHighlightDelta.options,
     name: 'multihighlightdelta',
-    version: '0.1'
+    version: '0.2'
   });
 
   // Nothing to wire since we're injecting the plugin inside flot
   return {};
-
 });
